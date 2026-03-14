f51a4c3f2ead798ed02c9a16c4abcd1d57ffaab1
Daniil Hebenton <daniil.hebenton@gmail.com>
2026-03-14 14:43:46 +0000

    EPBI

--- src/components/screens/analytics/cards/KPICards/KPICard.jsx
+++ src/components/screens/analytics/cards/KPICards/KPICard.jsx
@@ -7,9 +7,9 @@ export default function KPICard({label, score, heading, subheading }) {
                <p className="label-main font13 w540">{label}</p>
                <div className='f-row g14'>
                     <MetricCharts score={score}/>
-                    <div className="f-col g5">
+                    <div className="f-col g6">
                          <h3 className="font14 w660 heading ln12">{heading}</h3>
-                         <p className='font13 w540 subheading lh14'>{subheading}</p>
+                         <p className='font13 twoliner w540 subheading lh13'>{subheading}</p>
                     </div>
                </div>
           </div>
